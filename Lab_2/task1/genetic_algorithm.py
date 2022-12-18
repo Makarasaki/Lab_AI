aa6951438ec57958dabcb09ca9b7101215331fcf
Maksymilian <makaleder@gmail.com>
2022-12-18 15:28:03 +0100

    started genetic algorithm

--- Lab_2/task1/genetic_algorithm.py
+++ Lab_2/task1/genetic_algorithm.py
@@ -1,4 +1,42 @@
 #!/usr/bin/python3
 
+import random
+import pandas as pd
+
+NUMBER_OF_COLUMNS = 99
+NUMBER_OF_ROWS = 12
+NUMBER_OF_RESOURCES = 10
+
+def upload_tasks():
+    tasks_data = pd.read_excel("GA_task.xlsx")
+    return tasks_data
+
+def extract_products_instructions(tasks_data):
+    products = []
+    for column in range(0, NUMBER_OF_COLUMNS, 2):
+        products.append([])
+        for row in range(1, NUMBER_OF_ROWS):
+            products[int(column/2)].append([tasks_data.iloc[row, column], tasks_data.iloc[row, column + 1]])
+    return products
+
+def extract_resources_schedules(products):
+    resources = []
+    for _ in range(0, NUMBER_OF_RESOURCES):
+        resources.append([])
+    print(resources)
+    for product in products:
+        for job in product:
+            print(job)
+            print('product', product)
+            resources[job[0]-1].append([products.index(product)+1, job[1]])
+    # print('resources', resources)
+    for list in resources:
+        print('resource: ', list)
+        
+    
+
 if __name__ == '__main__':
-    pass
+    tasks_data = upload_tasks()
+    products = extract_products_instructions(tasks_data)
+    extract_resources_schedules(products)
+    
